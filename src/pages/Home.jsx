2ba1f0f82c56685f9cf135e7eafcc62b5b340cb9
Ahmedglez <ahmediglez@gmail.com>
2022-06-17 00:29:38 -0400

    fixing accordion #2

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -3,18 +3,17 @@ import Introduction from '../components/Introduction';
 import AboutUsComponent from '../components/AboutUsComponent';
 import ServicesComponent from '../components/ServicesComponent';
 import Accordion from '../containers/Accordion';
-import '../styles/Home.scss'
+import '../styles/Home.scss';
 
 const Home = () => {
-    return (
-        <div className='home-container container-fluid flex-column align-content-center p-0'>
-           <Introduction />
-           <AboutUsComponent />
-           <ServicesComponent />
-           <Accordion />
-
-        </div>
-    );
-};  
+	return (
+		<div className='home-container container-fluid flex-column align-content-center p-0'>
+			<Introduction />
+			<AboutUsComponent />
+			<Accordion />
+			<ServicesComponent />
+		</div>
+	);
+};
 
 export default Home;
